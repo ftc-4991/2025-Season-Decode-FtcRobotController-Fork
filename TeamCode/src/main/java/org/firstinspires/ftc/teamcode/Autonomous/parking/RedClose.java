f26dbe4b82cc4b9ecf1ebe56b95f9a833457d18a
GearFreaks <ftc4991@gmail.com>
2025-10-16 17:40:25 -0400

    4 new autonomous programs park via strafing left or right. (4 starting positions)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/parking/RedClose.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.Autonomous.parking;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ *
+ * Close refers to the larger launch zone, not the proximity to the drivers
+ *
+ */
+@Autonomous
+public class RedClose extends LinearOpMode {
+    DcMotor fr = null;
+    DcMotor fl = null;
+    DcMotor br = null;
+    DcMotor bl = null;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        fr = hardwareMap.get(DcMotor.class,"fr");
+        fl = hardwareMap.get(DcMotor.class,"fl");
+        br = hardwareMap.get(DcMotor.class,"br");
+        bl = hardwareMap.get(DcMotor.class,"bl");
+
+        fl.setDirection(DcMotorSimple.Direction.REVERSE);
+        bl.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        double power = 1;
+
+        fr.setPower(-power);
+        fl.setPower(power);
+        br.setPower(power);
+        bl.setPower(-power);
+
+        sleep (3000);
+
+        fr.setPower(0);
+        fl.setPower(0);
+        br.setPower(0);
+        bl.setPower(0);
+    }
+}
